51f548a6a7a1160b4f347d98cd635f877701da05
ghost <ghostpy91.com>
2025-08-09 06:13:43 +0600

    frontend & auth

---
+++ src/components/Footer.jsx
@@ -0,0 +1,61 @@
+import { Link } from 'react-router-dom'
+import { FaFacebook, FaInstagram, FaPinterest, FaTwitterSquare } from 'react-icons/fa'
+
+const Footer = () => {
+  return (
+    <footer className='bg-gray-900 text-gray-200 py-10'>
+      <div className='max-w-7xl mx-auto px-4 md:flex md:justify-between'>
+        <div className='mb-6 md:mb-0'>
+            <Link to='/'>
+              <img src='logo2.png' alt="" className='w-20 absolute'/>
+              <h2 className='text-yellow-500 relative pt-15'>NEOGEARS</h2>
+            </Link>
+            <p className='mt-2 text-sm'>Powering Your World with the Best in Electronics.</p>
+            <p className='mt-2 text-sm'>123 Electronics St, Rajshahi City, NY 10010</p>
+            <p className='text-sm'>Email: support@neogars.com</p>
+            <p className='text-sm'>Phone: (123) 909-9900</p>
+        </div>
+        {/* customer service link */}
+        <div className='mb-6 md:mb-0'>
+            <h3 className='text-xl font-semibold'>Customer Service</h3>
+            <ul className='mt-2 text-sm space-y-2'>
+                <li>Contact Us</li>
+                <li>Shipping & Returns</li>
+                <li>FAQs</li>
+                <li>Order Tracking</li>
+                <li>Size Guide</li>
+            </ul>
+        </div>
+        {/* social media links */}
+        <div className='mb-6 md:mb-0'>
+            <h3 className='text-xl font-semibold'>Follow Us</h3>
+            <div className='flex space-x-4 mt-2'>
+                <FaFacebook/>
+                <FaInstagram/>
+                <FaTwitterSquare/>
+                <FaPinterest/>
+            </div>
+        </div>
+        {/* newsletter subscription */}
+        <div>
+            <h3 className='text-xl font-semibold'>Stay in the Loop</h3>
+            <p className='mt-2 text-sm'>Subscribe to get special offers, free giveaways, and more</p>
+            <form action="" className='mt-4 flex'>
+                <input 
+                type="email" 
+                placeholder='Your email address'
+                className='w-full p-2 rounded-l-md  text-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-500'
+                />
+                <button type='submit' className='bg-[#FF4F0F] cursor-pointer text-white px-4 rounded-r-md hover:bg-[#FF4F0F]'>Subscribe</button>
+            </form>
+        </div>
+      </div>
+      {/* bottom section */}
+      <div className='mt-8 border-t border-gray-700 pt-6 text-center text-sm'>
+        <p>&copy; {new Date().getFullYear()} <span className='text-yellow-500 text-[17px]'>NeoGears</span>. All rights reserved</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
